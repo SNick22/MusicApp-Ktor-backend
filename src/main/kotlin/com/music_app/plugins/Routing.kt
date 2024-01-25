f6c9765c30b527855c03962bf7e2e91839b45c15
SNick22 <azat3310977@gmail.com>
2024-01-25 18:05:26 +0300

    initial commit

---
+++ src/main/kotlin/com/music_app/plugins/Routing.kt
@@ -0,0 +1,13 @@
+package com.music_app.plugins
+
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Application.configureRouting() {
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+    }
+}
---
+++ src/test/kotlin/com/music_app/ApplicationTest.kt
@@ -0,0 +1,4 @@
+package com.music_app
+
+class ApplicationTest {
+}
